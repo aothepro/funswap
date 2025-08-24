94fc47d96b795e24bf6a0746dba99a4ade208d8d
Ashraf Omar <ao356@live.com>
2025-08-24 21:53:17 +0100

    added gradient pipe between cards

--- src/components/TokenSwapCompareSection/TokenSwapCompareSection.tsx
+++ src/components/TokenSwapCompareSection/TokenSwapCompareSection.tsx
@@ -3,12 +3,16 @@
 import { useContext } from "react";
 import { CardType, TokenCard } from "./TokenCard";
 import { TokenSwapContext } from "../TokenSwap/context/TokenSwapContext";
+import styles from "./styles.module.css";
 
 export const TokenSwapCompareSection = () => {
   const { from, to } = useContext(TokenSwapContext);
   return (
-    <div className="flex gap-3 p-5 justify-center">
+    <div className="flex p-5 justify-center">
       <TokenCard token={from} cardType={CardType.FROM} />
+      <div className="min-w-32 grow h-2 overflow-hidden my-auto border border-gray-200">
+        <div className={`${styles.pipeline} w-full h-full bg-blue-500`}></div>
+      </div>
       <TokenCard token={to} cardType={CardType.TO} />
     </div>
   );
