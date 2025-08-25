f24cda44ddc8ee607816c71dfcbba01a50da462e
Ashraf Omar <ao356@live.com>
2025-08-25 12:22:06 +0100

    improve loading state for better visual

--- src/components/TokenSwapCompareSection/TokenCard.tsx
+++ src/components/TokenSwapCompareSection/TokenCard.tsx
@@ -169,10 +169,10 @@ export const TokenCard = ({
             </div>
             <div className="font-normal text-gray-700 dark:text-gray-400">
               {inFlight ? (
-                <div className="flex gap-2 h-12 my-auto">
+                <div className="flex gap-2 h-12 my-auto backdrop-blur-3xl p-2 rounded-full outline-1">
                   <Spinner />
-                  <div className="my-auto text-white">
-                    Fetching latest price...
+                  <div className="my-auto text-lg font-bold leading-none tracking-tight text-white pe-3">
+                    Fetching latest price
                   </div>
                 </div>
               ) : (
