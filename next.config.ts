95b85aa1c957afa5843e41f909d4c152a4cf2d73
Ashraf Omar <ao356@live.com>
2025-08-25 08:40:02 +0100

    use higher resolution icon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://raw.githubusercontent.com/spothq/cryptocurrency-icons/refs/heads/master/32/icon/**.png')],
+    remotePatterns: [new URL('https://raw.githubusercontent.com/spothq/cryptocurrency-icons/refs/heads/master/[0-9]+/icon/**.png')],
   },
 };
 
--- src/utils/getTokenImage.ts
+++ src/utils/getTokenImage.ts
@@ -1,4 +1,4 @@
 
 export const getTokenImage = (symbol: string): string => {
-    return `https://raw.githubusercontent.com/spothq/cryptocurrency-icons/refs/heads/master/32/icon/${symbol.toLowerCase()}.png`
+    return `https://raw.githubusercontent.com/spothq/cryptocurrency-icons/refs/heads/master/128/icon/${symbol.toLowerCase()}.png`
 };
